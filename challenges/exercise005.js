cd8d1917e1e5765f550c3042b09df46dbad8fb14
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2022-11-29 22:59:37 +0000

    Exercise 4 and part of 5

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,12 +1,20 @@
 export const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  const nIndex = nums.indexOf(n);
+  if (nIndex === nums.length - 1 || nIndex === -1) {
+    return null;
+  } else {
+    return nums[nIndex + 1];
+  }
 };
 
 export const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let count = {0: 0, 1: 0};
+  count['0'] = str.split('0').length - 1;
+  count['1'] = str.split('1').length - 1;
+  return count;
 };
 
 export const reverseNumber = (n) => {
